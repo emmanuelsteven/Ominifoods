f1786623d0a152b9719a1414056a99fe7dd862c9
Emmanuel Steven <chizzyjiver@gmail.com>
2024-09-23 12:05:19 +0100

    sticky navigation

--- js/script.js
+++ js/script.js
@@ -37,6 +37,34 @@ allLinks.forEach(function (link) {
   });
 });
 
+// sticky nav
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+    // if (!ent.isIntersecting) {
+    //   document.body.classList.add("sticky");
+    // }
+    // if (ent.isIntersecting) {
+    //   document.body.classList.remove("sticky");
+    // }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
